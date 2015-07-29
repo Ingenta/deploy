668ce49332629c1e2b1d5d443f89a0f9d7c8ef88
baidan4855 <baidan4855@qq.com>
2015-07-29 14:51:54 +0800

    ....

--- science/setup_mongo.sh
+++ science/setup_mongo.sh
@@ -2,16 +2,19 @@
 docker rm -f node1
 docker rm -f node2
 
-rm -rf /volume/mongo/db/rs1/*
-rm -rf /volume/mongo/db/rs2/*
-rm -rf /volume/mongo/log/*
+rm -rf /docker_volume/mongo/db/rs1/*
+rm -rf /docker_volume/mongo/db/rs2/*
+rm -rf /docker_volume/mongo/log/*
 
-mkdir -p /volume/mongo/db/rs1
-mkdir -p /volume/mongo/db/rs2
-mkdir -p /volume/mongo/log
+mkdir -p /docker_volume/mongo/db/rs1
+mkdir -p /docker_volume/mongo/db/rs2
+mkdir -p /docker_volume/mongo/log
 
-touch /volume/mongo/log/rs1.log
-touch /volume/mongo/log/rs2.log
+touch /docker_volume/mongo/log/rs1.log
+touch /docker_volume/mongo/log/rs2.log
 
-docker run -d --name node1 -p 27017:27017 -v /volume/mongo/db/rs1:/data/db -v /volume/mongo/log:/data/log mongo --logpath /data/log/rs1.log --oplogSize 100 --replSet rs/192.168.1.2:27018 --journal
-docker run -d --name node2 -p 27018:27017 -v /volume/mongo/db/rs2:/data/db -v /volume/mongo/log:/data/log mongo --logpath /data/log/rs2.log --oplogSize 100 --replSet rs/192.168.1.2:27017 --journal
+chgrp -R docker /docker_volume/mongo
+chmod -R g+w /docker_volume/mongo
+
+docker run -d --name node1 -p 27017:27017 -v /docker_volume/mongo/db/rs1:/data/db -v /docker_volume/mongo/log:/data/log mongo --logpath /data/log/rs1.log --oplogSize 100 --replSet rs/192.168.1.2:27018 --journal
+docker run -d --name node2 -p 27018:27017 -v /docker_volume/mongo/db/rs2:/data/db -v /docker_volume/mongo/log:/data/log mongo --logpath /data/log/rs2.log --oplogSize 100 --replSet rs/192.168.1.2:27017 --journal
