da7b67f5a0a04e4417799cb0c88f391033307d1a
baidan4855 <baidan4855@qq.com>
2015-07-29 14:57:24 +0800

    111

--- science/setup_mongo.sh
+++ science/setup_mongo.sh
@@ -15,6 +15,7 @@ touch /docker_volume/mongo/log/rs2.log
 
 chgrp -R docker /docker_volume/mongo
 chmod -R g+w /docker_volume/mongo
+chmod o+w /docker_volume/mongo/log/*
 
 docker run -d --name node1 -p 27017:27017 -v /docker_volume/mongo/db/rs1:/data/db -v /docker_volume/mongo/log:/data/log mongo --logpath /data/log/rs1.log --oplogSize 100 --replSet rs/192.168.1.2:27018 --journal
 docker run -d --name node2 -p 27018:27017 -v /docker_volume/mongo/db/rs2:/data/db -v /docker_volume/mongo/log:/data/log mongo --logpath /data/log/rs2.log --oplogSize 100 --replSet rs/192.168.1.2:27017 --journal
